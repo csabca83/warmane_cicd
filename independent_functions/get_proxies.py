db12bce1a8d4978232c46f4fc16f023697571e57
unknown <jagermeister1888@gmail.com>
2021-08-31 11:41:59 +0200

    realised that socks wasnt isntalled for requests

--- independent_functions/get_proxies.py
+++ independent_functions/get_proxies.py
@@ -16,7 +16,7 @@ def get_proxies():
                "&"
                "protocol=socks4"
                "&"
-               "timeout=10000"
+               "timeout=100"
                "&"
                "country=all"
                "&"
